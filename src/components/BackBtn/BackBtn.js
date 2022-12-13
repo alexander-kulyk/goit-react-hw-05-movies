cfbc80d52da060a3849e9dfdbfcb0c8b6eebea34
alexander-kulyk <kulyk.alexander@gmail.com>
2022-12-13 15:26:31 +0200

    Update BackBtn.js

--- src/components/BackBtn/BackBtn.js
+++ src/components/BackBtn/BackBtn.js
@@ -5,7 +5,7 @@ import { ButtonBack } from "./BackBtn.styled"
 export const BackBtn = ({location}) =>{
     console.log(location)
     return(
-        <ButtonBack  to={location.state?.from} >Go Back</ButtonBack>
+        <ButtonBack  to={location.state?.from ?? "/"} >Go Back</ButtonBack>
     )
 }
 
